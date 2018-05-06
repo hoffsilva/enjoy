a663770e07118c2278172e949cc603a25b5b0a13
Hoff Silva <hoff.henry@gmail.com>
2018-05-06 02:57:18 -0300

    Initial Commit

---
+++ Laud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Laud
+//
+//  Created by Hoff Henry Pereira da Silva on 06/05/2018.
+//  Copyright © 2018 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
